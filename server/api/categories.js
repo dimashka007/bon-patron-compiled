ade4c40e6adf57d3edd9ab7de31ee9a213d637e0
demilotion007 <demilotion007@gmail.com>
2020-06-11 13:44:06 +0300

    initial commit

--- server/api/categories.js
+++ server/api/categories.js
@@ -3,6 +3,7 @@ const mongodb = require('mongodb');
 const router = express.Router();
 
 router.get('/api/categories', async (req, res) => {
+  console.log('Get Categories')
   const categories = await loadCollection();
   const current = categories.db('bon-patron').collection('categories');
   res.send(await current.find({}).toArray());
